a11a65e56cf8e524165252584407edd56e409eed
jhonpulido1990 <3825@holbertonschool.com>
2022-05-24 15:06:57 -0500

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def task_0():
+    """returns Hello HBNB!"""
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb', strict_slashes=False)
+def task_1():
+    """returns HBNB"""
+    return ('HBNB')
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def task_2(text):
+    """returns params text"""
+    return ('C ' + text.replace('_', ' '))
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def task_3(text='is cool'):
+    """return default value is cool else params text"""
+    return ('Python ' + text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def task_4(n):
+    """return param how a number"""
+    return ('{} is a number'.format(n))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
